42bc3373ad2d289f7e44b18a61c3e80041cece2f
ritchie20 <oscar.rubio.ma@gmail.com>
2016-01-26 16:02:41 -0300

    lamp stack basic working

--- files/wordpressdb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mysql -u root -phandl3bur -e "create user 'wordpress'@'localhost' IDENTIFIED BY 'wordpresspass'; create database if not exists wordpressdb; grant all privileges on wordpressdb.* to 'wordpress'@'localhost';"
